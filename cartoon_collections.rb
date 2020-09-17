3654105e3d03c15d007a25163958dcb998023115
Michelle Ranly <meranly8@gmail.com>
2020-09-17 04:35:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,11 +11,15 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls
 end
 
-def long_planeteer_calls(array)
-  
+def long_planeteer_calls(calls)
+  calls.all? do |call|
+    call.length == 4 
+  end
 end
 
 def find_the_cheese()
   
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
