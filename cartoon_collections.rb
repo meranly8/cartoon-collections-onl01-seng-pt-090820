b64e871a3431755e25e0093c5d04ac9a8d2b4c64
Michelle Ranly <meranly8@gmail.com>
2020-09-17 04:44:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,12 +15,13 @@ def long_planeteer_calls(calls)
   calls.any? do |call|
     call.length > 4
   end
-  
 end
 
-def find_the_cheese()
-  
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |cheese|
+    cheese == cheese_types
+  end
 end
 
 
