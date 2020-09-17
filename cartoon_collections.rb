0f9605f8ca963d62cf3c81658bc114632c1c62b9
Michelle Ranly <meranly8@gmail.com>
2020-09-17 04:41:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,14 +12,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.all? do |call|
-    if call.length >= 4 
-      true
-    else
-      false
-    end
+  calls.any? do |call|
+    call.length > 4
   end
-  calls.all?
+  
 end
 
 def find_the_cheese()
