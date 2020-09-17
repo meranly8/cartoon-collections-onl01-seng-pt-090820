c5019cc5f6e224e53e9ec5276e62852a505fdaec
Michelle Ranly <meranly8@gmail.com>
2020-09-17 05:04:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|element| element.capitalize + '!'}
+  planeteer_calls.collect {|element| element.capitalize + "!"}
 end
 
 def long_planeteer_calls(calls)
